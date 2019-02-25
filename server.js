af8c025a82832180c05a2d72421323927cf0d1cb
Sharath-Jayson <jaisarat@gmail.com>
2019-02-25 21:38:19 +0530

    updates

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/task-manager'));
+app.use(express.static('./dist/task-manager'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/task-manager/index.html'));
+res.sendFile(path.join(__dirname,'/dist/task-manager/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
