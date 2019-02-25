17b40e1486c30e2de84ee25062d12e3c90114ba8
Sharath-Jayson <jaisarat@gmail.com>
2019-02-25 22:10:35 +0530

    heroku retry

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/task-manager'));
@@ -13,3 +26,4 @@ res.sendFile(path.join(__dirname,'/dist/task-manager/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
